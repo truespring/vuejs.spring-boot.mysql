9cd069e5ed70a4d669ddae49a9431c4a43d9ea95
truespring <truespring1@gmail.com>
2022-09-20 00:48:33 +0900

    combine build process

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,4 +1,33 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+module.exports = {
+  outputDir: "../src/main/resources/static",
+  indexPath: "../templates/index.html",
+  devServer: {
+    port: 3000,
+    proxy: {
+      '/v1': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      }
+    }
+  },
+  // build: {
+  //   index: path.resolve(__dirname, '../src/main/resources/static'),
+  //   assetsRoot: path.resolve(__dirname, '../src/main/resources/static'),
+  //   assetsSubDirectory: 'static',
+  //   assetsPublicPath: '/',
+  //
+  //   productionSourceMap: true,
+  //   devtool: '#source-map',
+  //
+  //   productionGzip: false,
+  //   productionGzipExtensions: ['js', 'css'],
+  //
+  //   bundleAnalyzerReport: process.env.npm_config_repot
+  // }
+}
+
